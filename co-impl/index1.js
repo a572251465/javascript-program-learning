bd62cbf593a87dd0cfaa3b1b7e211f69c88cc76d
lihaohao <2495041749@qq.com>
2022-01-08 09:56:41 +0800

    feat: 实现co的原理

---
+++ co-impl/index1.js
@@ -0,0 +1,33 @@
+const makePromise = (time) =>
+  new Promise((resolve) => {
+    let timer = setTimeout(() => {
+      resolve(time)
+      clearTimeout(timer)
+    }, time)
+  })
+
+const iterator = function* () {
+  let res = yield makePromise(1000)
+  res = yield 2
+  res = yield makePromise(2000)
+  return res
+}
+
+function co(iterator) {
+  const it = iterator()
+  return new Promise((resolve) => {
+    function next(val) {
+      const { done, value } = it.next(val)
+      if (done) {
+        resolve(value)
+      } else {
+        next(value)
+      }
+    }
+    next()
+  })
+}
+
+co(iterator).then(res => {
+  console.log(res)
+})
