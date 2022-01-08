f4478261e272fff98a638ff7147e3b1a24355e76
lihaohao <2495041749@qq.com>
2022-01-08 10:41:57 +0800

    feat: 判断两个相同的树

--- leetcode/树/100. 相同的树/index.js
+++ leetcode/树/100. 相同的树/index.js
@@ -12,9 +12,8 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-  if (p && q === null) return false
-  if (p === null && q) return false
   if (p === null && q === null) return true
+  if (p === null || q === null) return false
 
   const left = isSameTree(p.left, q.left)
   const right = isSameTree(p.right, q.right)
