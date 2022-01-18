7c5cbeebaa9d1226ff4c7eaed4a4df462c8f1c06
lihaohao <2495041749@qq.com>
2022-01-19 07:56:03 +0800

    feat: 第N个泰波那契数列

--- leetcode/动态规划/1137. 第 N 个泰波那契数/index1.js
+++ leetcode/动态规划/1137. 第 N 个泰波那契数/index1.js
@@ -7,5 +7,5 @@ var tribonacci = function (n, memo = {}) {
   if (n === 2) return 1
   if (memo[n]) return memo[n]
 
-  return (memo[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3))
+  return (memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo))
 }
