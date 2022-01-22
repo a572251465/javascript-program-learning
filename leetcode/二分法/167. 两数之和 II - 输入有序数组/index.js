1ac3cb0a37f0aa6659e43519ab1442b2cfd2d89c
lihaohao <2495041749@qq.com>
2022-01-22 17:33:23 +0800

    feat: 167. 两数之和 II - 输入有序数组

--- leetcode/二分法/167. 两数之和 II - 输入有序数组/index.js
+++ leetcode/二分法/167. 两数之和 II - 输入有序数组/index.js
@@ -4,5 +4,33 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-  
+  let left = 0
+  let right = numbers.length
+
+  // [left, right)
+  while (left < right) {
+    const i = (left + (right - left) / 2) | 0
+    const mid = numbers[i]
+
+    if (mid > target) {
+      right = i
+    } else {
+      left = i + 1
+    }
+  }
+
+  const surplus = left < 2 ? numbers : numbers.slice(0, left)
+  const map = {}
+  for (let i = 0; i < surplus.length; i += 1) {
+    const item = surplus[i]
+    if (Reflect.has(map, item)) return [+map[item], i + 1]
+
+    map[target - item] = i + 1
+  }
+
+  return [0, 0]
 }
+
+// console.log(twoSum([2, 7, 11, 15], 9))
+// console.log(twoSum([2, 3, 4], 6))
+console.log(twoSum([-1, 0], -1))
