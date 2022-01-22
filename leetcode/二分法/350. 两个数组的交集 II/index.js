90c4788410b5608afd0b4a96b47a7801d314afac
lihaohao <2495041749@qq.com>
2022-01-22 22:57:38 +0800

    feat: 350. 两个数组的交集 II

---
+++ leetcode/二分法/350. 两个数组的交集 II/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  if (nums1.length === 0 || nums2.length === 0) return []
+
+  const map = {}
+  nums1.forEach((item) => {
+    if (Reflect.has(map, item)) {
+      map[item] = ++map[item]
+    } else {
+      map[item] = 1
+    }
+  })
+
+  const totals = []
+  nums2.forEach((item) => {
+    if (map[item]) {
+      totals.push(item)
+      map[item] = --map[item]
+    }
+  })
+
+  return totals
+}
