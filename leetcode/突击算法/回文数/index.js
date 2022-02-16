02516f6812e2e9234cbc65b890e6b35ed126121e
lihaohao <2495041749@qq.com>
2022-02-16 21:08:23 +0800

    feat: 突击算法

---
+++ leetcode/突击算法/回文数/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x !== Math.abs(x)) return false
+
+  let res = 0
+  let y = x
+  while (y !== 0) {
+    const expect = y % 10
+    y = (y / 10) | 0
+
+    res = res * 10 + expect
+  }
+
+  return res === x
+}
+
+console.log(isPalindrome(121))
