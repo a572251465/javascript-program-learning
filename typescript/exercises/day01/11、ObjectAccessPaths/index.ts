803dbfacad4e4d60ddca104deb07e499df1b4db1
lihaohao <2495041749@qq.com>
2022-05-02 23:28:02 +0800

    feat: 重构目录è§结构

--- typescript/exercises/day01/11、ObjectAccessPaths/index.ts
+++ typescript/exercises/day01/11、ObjectAccessPaths/index.ts
@@ -6,11 +6,6 @@ type ObjectAccessPaths<T, R extends string = '', K = keyof T> = K extends keyof 
   ) : never
 ) : never
 
-// 得到联合类型：
-/*
-home.topBar.title | home.topBar.welcome | home.bottomBar.notes | login.username | login.password
-*/
-
 // 完成 createI18n 函数中的 ObjectAccessPaths<Schema>，限制函数i18n的参数为合法的属性访问字符串
 function createI18n<Schema>(schema: Schema): ((path: ObjectAccessPaths<Schema>) => string) {return [{schema}] as any}
 
@@ -34,8 +29,5 @@ const i18n = createI18n({
 i18n('home.topBar.title')           // correct
 i18n('home.topBar.welcome')         // correct
 i18n('home.bottomBar.notes')        // correct
-
-// i18n('home.login.abc')              // error，不存在的属性
-// i18n('home.topBar')                 // error，没有到最后一个属性
 
 export default {}
